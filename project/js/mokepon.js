1e99591251a43ec7ad5a25b52f6fd68bbaed29c9
Sebas2 Pérez2 <Jspc513@gmail.com>
2024-08-25 22:47:24 -0500

    Usando canvas (saquenme de aqui)

--- project/js/mokepon.js
+++ project/js/mokepon.js
@@ -20,6 +20,9 @@ const ataquesDelEnemigo = document.getElementById('ataques-del-enemigo')
 const contenedorBoxes = document.getElementById('contenedorBoxes')
 const contenedorAtaques = document.getElementById('contenedorAtaques')
 
+const sectionVerMapa = document.getElementById('ver-mapa')
+const mapa = document.getElementById('mapa')
+
 let mokepones = []
 let ataqueJugador  = []
 let ataqueEnemigo = []
@@ -40,6 +43,7 @@ let victoriasJugador = 0
 let victoriasEnemigo = 0
 let vidasJugador = 3
 let vidasEnemigo = 3
+let lienzo = mapa.getContext("2d")
 
 class Mokepon {
     constructor(nombre, foto, vidas){
@@ -85,6 +89,7 @@ mokepones.push(hipodoge, capipepo, ratigueya)
 function iniciarJuego() {
 
     sectionSeleccionarAtaque.style.display = 'none'
+    sectionVerMapa.style.display = 'none'
 
     mokepones.forEach((mokepon) => {
         opcionesMokepones = `
@@ -111,8 +116,17 @@ function seleccionarMascotaJugador() {
     
     sectionSeleccionarMascota.style.display = 'none'
     
-    
-    sectionSeleccionarAtaque.style.display = 'flex'
+    //sectionSeleccionarAtaque.style.display = 'flex'
+    sectionVerMapa.style.display = 'flex'
+    let imagenDeCapipepo = new Image()
+    imagenDeCapipepo.src = capipepo.foto
+    lienzo.drawImage(
+        imagenDeCapipepo,
+        20,
+        40,
+        100,
+        100
+    )
 
     if (inputHipodoge.checked){
         spanMascotaJugador.innerHTML = inputHipodoge.id
